b10d060d1d87c8275ba496e2dba010346aa30c42
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-05 12:40:11 -0300

    Teste de acesso valindando o nível de permissão do gerente

--- src/test/java/proxy/RelatorioFinanceiroProxyTest.java
+++ src/test/java/proxy/RelatorioFinanceiroProxyTest.java
@@ -23,4 +23,13 @@ class RelatorioFinanceiroProxyTest {
         assertEquals(Arrays.asList("Data: 2026-06-04", "Status: Caixa verificado"), proxy.obterResumoDiario());
     }
 
+
+    @Test
+    void devePermitirQueGerenteVejaOFaturamento() {
+        Usuario gerente = new Usuario("Marco Antônio (Dono)", true);
+        RelatorioFinanceiroProxy proxy = new RelatorioFinanceiroProxy("2026-06-04");
+
+        assertEquals(Arrays.asList(1500.50, 450.00), proxy.obterFaturamentoDetalhado(gerente));
+    }
+
 }
