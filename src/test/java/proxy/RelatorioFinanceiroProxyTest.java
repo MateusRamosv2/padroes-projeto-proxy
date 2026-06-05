e19e926060e697eef2f9902ad66e433b31275abb
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-05 12:38:19 -0300

    Implementando teste de acesso geral - Sem restrição de acesso por user

---
+++ src/test/java/proxy/RelatorioFinanceiroProxyTest.java
@@ -0,0 +1,26 @@
+package proxy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RelatorioFinanceiroProxyTest {
+
+    @BeforeEach
+    void setUp() {
+
+        BDRestaurante.addRelatorio("2026-06-04", new RelatorioFinanceiroReal("2026-06-04", "CAIXA BATIDO", 1500.50, 450.00));
+    }
+
+    @Test
+    void devePermitirQueQualquerUmVejaOResumoDiario() {
+        RelatorioFinanceiroProxy proxy = new RelatorioFinanceiroProxy("2026-06-04");
+
+
+        assertEquals(Arrays.asList("Data: 2026-06-04", "Status: Caixa verificado"), proxy.obterResumoDiario());
+    }
+
+}
